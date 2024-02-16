180060ca7b70cf23f8e06d15c22a0b37c21c927d
rubyruivivar <159798798+rubyruivivar@users.noreply.github.com>
2024-02-16 14:17:52 +0800

    Update Guardian_of_Archipelago.java

--- Guardian_of_Archipelago.java
+++ Guardian_of_Archipelago.java
@@ -56,11 +56,10 @@ class Routes {
                     "Imber Cove ---> Autumnvale ---> Frostpeak ---> Aquaville",
                     "Imber Cove ---> Autumnvale ---> Kindlewood ---> Aquaville" }
     };
-    return allRoutes1[islandIndex1 - 1];
-}
+        return allRoutes1[islandIndex1 - 1];
+    }
 
     public static String[] routesAutumnvale(int islandIndex2) { // from Autumnvale
-        // Define and return possible routes based on the island index
         String[][] allRoutes2 = {
                 { "Autumnvale ---> Frostpeak", "Autumnvale ---> Kindlewood ---> Aquaville ---> Frostpeak" },
                 { "Autumnvale ---> Kindlewood", "Autumnvale ---> Frostpeak ---> Aquaville ---> Kindlewood" },
@@ -70,7 +69,6 @@ class Routes {
     }
     
     public static String[] routesFrostpeak(int islandIndex3) { // from Frostpeak
-        // Define and return possible routes based on the island index
         String[][] allRoutes3 = {
             { "Frostpeak ---> Autumnvale", "Frostpeak ---> Aquaville ---> Kindlewood ---> Autumnvale" },
             { "Frostpeak ---> Autumnvale ---> Kindlewood", "Frostpeak ---> Aquaville ---> Kindlewood" },
@@ -80,7 +78,6 @@ class Routes {
     }
     
     public static String[] routesKindlewood(int islandIndex4) { //from Kindlewood
-        // Define and return possible routes based on the island index
         String[][] allRoutes4 = {
             { "Kindlewood ---> Autumnvale", "Kindlewood ---> Aquaville ---> Frostpeak ---> Autumnvale" },
             { "Kindlewood ---> Autumnvale ---> Frostpeak", "Kindlewood ---> Aquaville ---> Frostpeak" },
@@ -90,13 +87,138 @@ class Routes {
     }
     
     public static String[] routesAquaville(int islandIndex5) { //from Aquaville
-        // Define and return possible routes based on the island index
         String[][] allRoutes5 = {
             { "Aquaville ---> Frostpeak ---> Autumnvale", "Aquaville ---> Kindlewood ---> Autumnvale" },
             { "Aquaville ---> Frostpeak", "Aquaville ---> Kindlewood ---> Autumnvale ---> Frostpeak" },
             { "Aquaville ---> Kindlewood", "Aquaville ---> Frostpeak ---> Autumnvale ---> Kindlewood" },
         };
         return allRoutes5[islandIndex5 - 1];
+    }
+}
+
+class QuizGame {
+    private Scanner scan;
+    private Random random;
+    private String[] questions;
+    private String[] answers;
+    private Thread timerThread;
+
+    public QuizGame(String[] questions, String[] answers) {
+        this.scan = new Scanner(System.in);
+        this.random = new Random();
+        this.questions = questions;
+        this.answers = answers;
+    }
+    
+    public QuizGame() {
+        String[] defaultQuestions = {
+            "What is the capital of France?",
+            "Who wrote 'Romeo and Juliet'?",
+            "What is the boiling point of water in Celsius?",
+            "Which country is known as the Land of the Rising Sun?",
+            "Which planet is known as the ‘Red Planet’?",
+            "What do we call the pattern of stars that people imagine in the sky?",
+            "On what continent would you find the world’s largest desert?",
+            "Who was the first Disney princess?",
+            "Who is known as the “Father of Modern Physics”?",
+            "Who painted the famous artwork “The Last Supper”?",
+            "What is the name of the little mermaid in the Disney movie?",
+            "What animal is known for its long neck and spots on its body?",
+            "In what season do leaves fall off the trees?",
+            "What animal is known for its ability to change color to match its surroundings?",
+            "In what fairy tale does a pumpkin turn into a carriage?",
+            "What is the main color of the Smurfs?",
+            "In the nursery rhyme, who had a farm with a dog named BINGO?",
+            "Which bird can repeat words and phrases that it hears, often kept as a pet?",
+            "Google Chrome, Safari, Firefox, and Explorer are different types of what?",
+            "What part of the atom has no electric charge?",
+            "What is the symbol for potassium?",
+            "What is meteorology the study of?",
+            "Which planet is the hottest in the solar system?",
+            "Which natural disaster is measured with a Richter scale?",
+            "What animals are pearls found in?",
+            "Which planet has the most gravity?",
+            "What was the name of the actor who played Jack Dawson in Titanic?",
+            "Which cartoon character lives in a pineapple under the sea?",
+            "Which popular soft drink was originally created as a medicine in the 19th century?",
+            "What goes up and down but doesn’t move?",
+            "What has one eye, but can’t see?",
+            "What building has the most stories?",
+            "I am the beginning of everything, the end of everywhere. I'm the beginning of eternity, the end of time and space. What am I?",
+            "What can fill a room but takes up no space?",
+            "What kind of tree can you carry in your hand?",
+            "They come out at night without being called, and are lost in the day without being stolen. What are they?",
+            "What goes through cities and fields, but never moves?",
+            "When it is alive we sing, when it is dead we clap ourhands. What is it?",
+            "I have no life, but I can die. What am I?",
+            "What kind of room has no walls, door or windows?",
+            "I make a loud sound when I’m changing. When I do change, I get bigger but weigh less. What am I?",
+            "It has keys, but no locks. It has space, but no room. You can enter, but can’t go inside. What is it?",
+            "I’m orange, I wear a green hat and I sound like a parrot. What am I?",
+            "What thrives when you feed it but dies when you water it?",
+            "What has 13 hearts but no other organs?",
+            "What kind of ship has two mates but no captain?",
+            "Which word in the dictionary is spelled incorrectly?",
+            "What begins with an 'e' and only contains one letter?",
+            "Which fish costs the most?",
+            "What five-letter word becomes shorter when you add two letters to it?",
+            "What has a face and two hands but no arms or legs?",
+            "What has many keys but cannot open a single lock?",
+            "What's always found on the ground but never gets dirty?",
+            "What gets wet while drying?",
+            "What has a head and a tail but no body?",
+            "What is full of holes but still holds water?",
+            "What has many teeth but cannot bite?",
+            "What has one head, one foot, and four legs?",
+            "What's always running but never gets hot?",
+        };
+
+        String[] defaultAnswers = {"PARIS", "WILLIAM SHAKESPEARE", "100", "JAPAN", "MARS", "CONSTELLATION", "ANTARCTICA", "SNOW WHITE", "ALBERT EINSTEIN", "LEONARDO DA VINCI", "ARIEL", "GIRAFFE", "AUTUMN/FALL", "CHAMELEON", "CINDERELLA", "BLUE", "OLD MACDONALD", "PARROT", "WEB BROWSERS", "NEUTRON", "K", "WEATHER", "VENUS", "EARTHQUAKES", "OYSTERS", "JUPITER", "LEONARDO DICAPRIO", "SPONGEBOB SQUAREPANTS", "COCA-COLA", "STAIRCASE", "NEEDLE", "LIBRARY", "E", "LIGHT", "PALM", "STARS", "ROAD", "BIRTHDAY CANDLE", "BATTERY", "MUSHROOM", "POPCORN", "KEYBOARD", "CARROT", "FIRE", "DECK OF CARDS", "RELATIONSHIP", "INCORRECTLY", "ENVELOPE", "GOLDFISH", "SHORT", "CLOCK", "PIANO", "SHADOW", "TOWEL", "COIN", "SPONGE", "COMB", "BED", "REFRIGERATOR",
+        };
+
+        this.questions = defaultQuestions;
+        this.answers = defaultAnswers;
+        this.random = new Random();
+        this.scan = new Scanner(System.in);
+    }
+
+    public String getRandomQuestion() {
+        int index = random.nextInt(questions.length);
+        return questions[index];
+    }
+
+    public boolean checkAnswer(String userAnswer, int questionIndex) {
+        return userAnswer.equalsIgnoreCase(answers[questionIndex]);
+    }
+
+    public void startTimer(int seconds) {
+        timerThread = new Thread(() -> {
+            for (int i = seconds; i >= 0; i--) {
+                System.out.print("\r" + i + " seconds remaining");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("\nTimer stopped.");
+                    return;
+                }
+            }
+            System.out.println("\rTime's up!");
+            System.exit(0);
+        });
+
+        timerThread.start();
+    }
+
+    public void stopTimer() {
+        timerThread.interrupt();
+    }
+    
+    public String[] getQuestions() {
+        return questions;
+    }
+
+    public String[] getAnswers() {
+        return answers;
     }
 }
 
@@ -130,27 +252,27 @@ public class GuardianofArchipelago {
             switch (choice) {
                 case 1:
                     System.out.println("\nYou choose the island of " + locations[choice - 1] + "."); // autumnvale
-                    System.out.print(
-                            "\nDESCRIPTION: Autumn or Fall is the season on this island. In contrast to its beautiful season, strong to violent winds are what covers the entire island.\n");
+                    System.out.print("\nDESCRIPTION: Autumn or Fall is the season on this island. In contrast to its beautiful season, strong to violent winds are what covers the entire island.\n");
                     displayRoutes(1);
+                    onBoat();
                     break;
                 case 2:
                     System.out.println("\nYou choose the island of " + locations[choice - 1] + "."); // frostpeak
-                    System.out.println(
-                            "\nDESCRIPTION: An island of ice and snow. Frostpeak is the territory of wolves which has a keen sense of smell, hearing, and vision.\n");
+                    System.out.println("\nDESCRIPTION: An island of ice and snow. Frostpeak is the territory of wolves which has a keen sense of smell, hearing, and vision.\n");
                     displayRoutes(2);
+                    onBoat();
                     break;
                 case 3:
                     System.out.println("\nYou choose the island of " + locations[choice - 1] + "."); // kindlewood
-                    System.out.println(
-                            "\nDESCRIPTION: One of the Forbidden Islands known for its earthquake phenomenon and is best described for its weather condition - summer.\n");
+                    System.out.println("\nDESCRIPTION: One of the Forbidden Islands known for its earthquake phenomenon and is best described for its weather condition - summer.\n");
                     displayRoutes(3);
+                    onBoat();
                     break;
                 case 4:
                     System.out.println("\nYou choose the island of " + locations[choice - 1] + "."); // aquaville
-                    System.out.println(
-                            "\nDESCRIPTION: Aquaville is an island that frequently rains. The said island is known to be a home of sea monsters. Be careful with your encounters with them.\n");
+                    System.out.println("\nDESCRIPTION: Aquaville is an island that frequently rains. The said island is known to be a home of sea monsters. Be careful with your encounters with them.\n");
                     displayRoutes(4);
+                    onBoat();
                     break;
                 default:
                     System.out.println("Invalid input. Enter the number that are listed in the option only.");
@@ -197,6 +319,38 @@ public class GuardianofArchipelago {
         System.out.print("\nEnter the number of the route you want to take from the options: ");
         int chosenRoutes = scan.nextInt();
         System.out.println("The route you choose is: (" + chosenRoutes + ") " + distances[chosenRoutes - 1] + " meter - " + routes[chosenRoutes - 1]);
+    }
+        
+    public static void onBoat() {
+        System.out.println("\nThe boat where you at started sailing...");
+        System.out.println("\nWarning: You encounter an obstacle!\n");
+    
+        QuizGame quizGame = new QuizGame();
+    
+        System.out.println("Welcome! You have 60 seconds to answer the question.");
+        quizGame.startTimer(60);
+    
+        String question = quizGame.getRandomQuestion();
+        System.out.println(question);
+        int questionIndex = Arrays.asList(quizGame.getQuestions()).indexOf(question);
+        scan.nextLine(); // Consume the newline character
+        String userAnswer = scan.nextLine().toUpperCase();
+    
+        // Stop the timer thread when the user enters an answer
+        quizGame.stopTimer();
+    
+        if (questionIndex >= 0 && questionIndex < quizGame.getAnswers().length) {
+            if (quizGame.checkAnswer(userAnswer, questionIndex)) {
+                System.out.println("Your answer is: " + userAnswer);
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Your answer is: " + userAnswer);
+                System.out.println("Correct answer is: " + quizGame.getAnswers()[questionIndex]);
+                System.out.println("Incorrect!");
+            }
+        } else {
+            System.out.println("Invalid question index.");
+        }
     }
 
     public static void main(String[] args) {
